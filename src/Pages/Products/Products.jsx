47c87fcd945042d448068b9e01d36b8db90bd3d2
Amin7775 <amin.dpi24@gmail.com>
2023-09-21 19:35:25 +0600

    Added Photo gallery in home and loaded products in product page

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,9 +1,15 @@
+import { useLoaderData } from "react-router-dom";
+import ProductCard from "./ProductCard";
 
 
 const Products = () => {
+    const {products} = useLoaderData();
+    console.log(products)
     return (
-        <div>
-            this is products
+        <div className="grid grid-cols-3 gap-8 p-20 ">
+            {
+                products.map(product => <ProductCard key={product.id} product={product}></ProductCard>)
+            }
         </div>
     );
 };
--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,25 +1,34 @@
 import { Outlet } from "react-router-dom";
-
+import Footer from "../../Pages/Footer/Footer";
 
 const MainLayout = () => {
-    return (
+  return (
+    <div>
+      <section className="flex justify-between px-10 shadow-md py-7">
         <div>
-            <section className="flex justify-between px-10 shadow-md py-7">
-                <div>
-                    <h1 className="text-3xl font-bold">Amajhon</h1>
-                </div>
-                <nav className="text-lg font-medium">
-                    <ul className="flex gap-10">
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/products">Products</a></li>
-                        <li><a href="/dashboard">Dashboard</a></li>
-                        
-                    </ul>
-                </nav>
-            </section>
-                <Outlet></Outlet>
+          <h1 className="text-3xl font-bold">Amajhon</h1>
         </div>
-    );
+        <nav className="text-lg font-medium">
+          <ul className="flex gap-10">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/products">Products</a>
+            </li>
+            <li>
+              <a href="/dashboard">Dashboard</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+      <div className="min-h-screen">
+
+      <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayout;
